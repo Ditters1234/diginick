a2ed485a85c4eea5c3c4e89f8a949a11a9598d40
Nicholas Tinsley <Nicholas Tinsley>
2017-03-13 16:19:21 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,189 @@
+
+
+// Keep track of our socket connection
+var socket;
+var systems;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  systems = [];
+  // Start a socket connection to the server
+  // Some day we would run this server somewhere else
+  socket = io.connect('http://localhost:8080');
+  // We make a named event called 'mouse' and write an
+  // anonymous callback function
+  socket.on('mouse',
+    // When we receive data
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+	  particlePush(data.x, data.y);
+
+    }
+  );
+}
+
+function draw() {
+  background(51);
+  background(0);
+  for (i = 0; i < systems.length; i++) {
+    systems[i].run();
+    systems[i].addParticle();
+  }
+  if (systems.length==0) {
+    fill(255);
+    textAlign(CENTER);
+    textSize(32);
+    text("click mouse to add particle systems", width/2, height/2);
+  }
+}
+
+function particlePush(x,y){
+    this.p = new ParticleSystem(createVector(x, y));
+  systems.push(p);
+  if(systems.length > 2){
+	systems.shift();
+  }
+}
+
+function mousePressed() {
+  //this.p = new ParticleSystem(createVector(mouseX, mouseY));
+  //systems.push(p);
+  particlePush(mouseX,mouseY);
+  sendmouse(mouseX,mouseY);
+}
+
+
+//function mouseDragged() {
+  // Draw some white circles
+//  fill(0,255,255);
+//  noStroke();
+//  ellipse(mouseX,mouseY,80,80);
+  // Send the mouse coordinates
+//  sendmouse(mouseX,mouseY);
+//}
+
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+  
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse',data);
+}
+
+
+
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255.0;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function () {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function () {
+  if (this.lifespan < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var ParticleSystem = function (position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function () {
+  // Add either a Particle or CrazyParticle to the system
+  if (int(random(0, 2)) == 0) {
+    p = new Particle(this.origin);
+  }
+  else {
+    p = new CrazyParticle(this.origin);
+  }
+  this.particles.push(p);
+};
+
+ParticleSystem.prototype.run = function () {
+  for (var i = this.particles.length - 1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
+
+// A subclass of Particle
+
+function CrazyParticle(origin) {
+  // Call the parent constructor, making sure (using Function#call)
+  // that "this" is set correctly during the call
+  Particle.call(this, origin);
+
+  // Initialize our added properties
+  this.theta = 0.0;
+};
+
+// Create a Crazy.prototype object that inherits from Particle.prototype.
+// Note: A common error here is to use "new Particle()" to create the
+// Crazy.prototype. That's incorrect for several reasons, not least 
+// that we don't have anything to give Particle for the "origin" 
+// argument. The correct place to call Particle is above, where we call 
+// it from Crazy.
+CrazyParticle.prototype = Object.create(Particle.prototype); // See note below
+
+// Set the "constructor" property to refer to CrazyParticle
+CrazyParticle.prototype.constructor = CrazyParticle;
+
+// Notice we don't have the method run() here; it is inherited from Particle
+
+// This update() method overrides the parent class update() method
+CrazyParticle.prototype.update=function() {
+  Particle.prototype.update.call(this);
+  // Increment rotation based on horizontal velocity
+  this.theta += (this.velocity.x * this.velocity.mag()) / 10.0;
+}
+
+// This display() method overrides the parent class display() method
+CrazyParticle.prototype.display=function() {
+  // Render the ellipse just like in a regular particle
+  Particle.prototype.display.call(this);
+  // Then add a rotating line
+  push();
+  translate(this.position.x, this.position.y);
+  rotate(this.theta);
+  stroke(255,this.lifespan);
+  line(0,0,25,0);
+  pop();
+}
+
